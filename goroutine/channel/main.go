7e8674d8ae34bae0b9fee758e3eb3a7af69148ae
Andhana Utama <magicwarms@gmail.com>
2022-01-03 15:38:25 +0700

    feat(add git ignore): gitignore

--- goroutine/channel/main.go
+++ goroutine/channel/main.go
@@ -6,7 +6,8 @@ import (
 )
 
 func main() {
-	runtime.GOMAXPROCS(4)
+	numOfCores := runtime.NumCPU()
+	runtime.GOMAXPROCS(numOfCores)
 	// Cara pembuatan channel yaitu dengan menuliskan keyword 'make'
 	// dengan isi keyword 'chan' diikuti dengan tipe data channel yang diinginkan.
 	// var messages = make(chan string) // <-- ini buat channel baru nya
@@ -15,56 +16,59 @@ func main() {
 	// Pada kode di dibawah, parameter kedua fungsi make() adalah representasi jumlah buffer.
 	// Perlu diperhatikan bahwa nilai
 	// buffered channel dimulai dari 0. Ketika nilainya adalah 2 brarti jumlah buffer maksimal ada 3.
-	var messages = make(chan int, 3)
+	var messages = make(chan string, 1)
 
-	// var sayHelloTo = func(who string) {
-	// 	var data = fmt.Sprintf("hello %s", who)
-	// 	messages <- data
+	// dibawah ini function closure wak!
+	// var sayHelloTo = func(who int) {
+	// 	var data = who
+	// 	nombor <- data
 	// }
 
-	// go sayHelloTo("john wick")
-	// go sayHelloTo("ethan hunt")
-	// go sayHelloTo("jason bourne")
+	// go sayHelloTo(1)
+	// go sayHelloTo(2)
+	// go sayHelloTo(3)
 
-	// var message1 = <-messages
+	// message1 := <-nombor
 	// fmt.Println(message1)
 
-	// var message2 = <-messages
+	// message2 := <-nombor
 	// fmt.Println(message2)
 
-	// var message3 = <-messages
+	// message3 := <-nombor
 	// fmt.Println(message3)
 
 	// penjelasan kode dibawah
 	// jadi tersedia data array, looping dulu untuk assign name ke var channel messages diatas
-	// var names = []string{"1 john wick", "2 ethan hunt", "3 jason bourne"}
-	// for _, val := range names {
-	// 	var assignName = func(who string) {
-	// 		var data = fmt.Sprintf(who)
-	// 		messages <- data
-	// 	}
-	// 	// panggil function closure diatas lalu assign ke variable messages
-	// 	go assignName(val)
-	// }
+	var names = []string{"1 john wick", "2 ethan hunt", "3 jason bourne"}
+	for _, val := range names {
+		var assignName = func(who string) {
+			var data = fmt.Sprint(who)
+			messages <- data
+		}
+		// panggil function closure diatas lalu assign ke variable messages
+		go assignName(val)
+	}
 	// lalu looping data nya
 	// jadi yang aku lakuin ini adalah channel sebagai tipe data parameter
-	// for i := 0; i < len(names); i++ {
-	// 	sayHelloTo(messages)
+	for i := 0; i < len(names); i++ {
+		sayHelloTo(messages)
+	}
+	// nombor := make(chan int)
+	// for i := 1; i <= 5; i++ {
+	// 	fmt.Println("prepare send data:", i)
+	// 	nombor <- i
+	// 	receiveData(nombor)
+	// 	fmt.Println("data sent: ", i)
 	// }
 
-	for i := 1; i <= 5; i++ {
-		fmt.Println("send data:", i)
-		messages <- i
-		go receiveData(messages)
-	}
-	// // dikasih gini supaya proses nya bener2 selesai
+	// dikasih gini supaya proses nya bener2 selesai
 	// var input string
 	// fmt.Scanln(&input)
 }
 
-func receiveData(receive chan int) {
-	fmt.Println("receive data:", <-receive)
-}
+// func receiveData(receive chan int) {
+// 	fmt.Println("received data:", <-receive)
+// }
 
 func sayHelloTo(who chan string) {
 	fmt.Println("hello wak", <-who)
--- goroutine/main.go
+++ goroutine/main.go
@@ -14,12 +14,15 @@ func massivePrint(till int, message string) {
 func main() {
 	// Fungsi ini digunakan untuk menentukan
 	// jumlah core atau processor yang digunakan dalam eksekusi program.
-	runtime.GOMAXPROCS(4)
+	numOfCores := runtime.NumCPU()
+	runtime.GOMAXPROCS(numOfCores)
 	// Pembuatan goroutine baru ditandai dengan keyword go.
 	go massivePrint(5, "mamak")
 	massivePrint(5, "bapak")
 
 	var input string
+	// fungsi Scanln mengakibatkan proses jalannya aplikasi berhenti di baris itu (blocking)
+	// hingga user menekan tombol enter.
 	fmt.Scanln(&input)
 
 	fmt.Println(input)
