6c4f5365e1ebdb4b2c9317e0604d375a95c1a403
Andhana Utama <magicwarms@gmail.com>
2022-02-23 10:48:02 +0700

    feat(if else learn): done

--- if-else/main.go
+++ if-else/main.go
@@ -5,13 +5,12 @@ import (
 )
 
 func main() {
-	// value := 10
-	// if value%2 == 0 {
-	// 	fmt.Println("Angka ini genap wak")
-	// 	return
-	// }
+	value := 10
+	if (value % 2) == 0 {
+		fmt.Println("Angka ini genap wak")
+	}
 
-	// cara lain if else, short syntax
+	// cara lain if short statement, short syntax
 	// if num := 10; num%2 == 0 { //checks if number is even
 	// 	fmt.Println("Angka ini genap wak:", num)
 	// 	// return
@@ -21,13 +20,14 @@ func main() {
 	// fmt.Println("Angka: ", 292-122)
 
 	// Variabel temporary adalah variabel yang hanya bisa digunakan pada blok seleksi kondisi dimana ia ditempatkan saja
-	point := 8840.0
+	point := 10000.0
 	// Deklarasi variabel temporary hanya bisa dilakukan lewat metode type inference yang menggunakan tanda :=. Penggunaan keyword var disitu tidak diperbolehkan karena akan menyebabkan error.
-	if percent := point / 100; percent >= 100 {
-		fmt.Printf("%.1f%s perfect!\n", percent, "%")
-	} else if percent >= 70 {
-		fmt.Printf("%.1f%s good\n", percent, "%")
+	calculatePercent := point / 100
+	if calculatePercent >= 100 {
+		fmt.Printf("%.1f%s perfect!\n", calculatePercent, "%")
+	} else if calculatePercent >= 70 {
+		fmt.Printf("%.1f%s good\n", calculatePercent, "%")
 	} else {
-		fmt.Printf("%.1f%s not bad\n", percent, "%")
+		fmt.Printf("%.1f%s not bad\n", calculatePercent, "%")
 	}
 }
