7013af404a8f9bf520ef4a8bcf33a18b3ed79719
Andhana Utama <magicwarms@gmail.com>
2022-02-28 10:02:22 +0700

    refactor: learn function

--- function/main.go
+++ function/main.go
@@ -1,11 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"math"
-	"strings"
-)
-
 // parameter dan return type adalah optional di go lang function
 // func calculateBill(qty int, price int) int {
 // 	total := qty * price
@@ -15,11 +9,10 @@ import (
 
 // other way to create function in golang
 // kalo type param nya sama gak usah dibikin kayak param function diatas
-func calculateBill(qty, price int) int {
-	total := qty * price
-
-	return total
-}
+// func calculateBill(qty, price int) int {
+// 	// total := qty * price
+// 	return qty * price
+// }
 
 // example multiple return value result from function
 // func rectProps(length, width float64) (float64, float64) {
@@ -29,24 +22,37 @@ func calculateBill(qty, price int) int {
 // }
 
 // other way to create and function declaration as return values
-func rectProps(length, width float64) (area, perimeter float64) {
-	area = length * width
-	perimeter = (length + width) * 2
-	return //no explicit return value
-}
+// func rectProps(length, width float64) (area, perimeter float64) {
+// 	area = length * width
+// 	perimeter = (length + width) * 2
+// 	return //no explicit return value
+// }
 
-func printMessage(message string, arr []string) {
-	nameString := strings.Join(arr, " and ")
-	fmt.Println(message, nameString)
-}
+// func printMessage(message string, arr []string) {
+// 	nameString := strings.Join(arr, " and ")
+// 	fmt.Println(message, nameString)
+// }
 
-func divideValue(v1, v2 float64) float64 {
-	if v1 == 0 {
-		fmt.Println("Nilai pembagian tidak boleh NOL!")
-		return 0
+// func divideValue(v1, v2 float64) float64 {
+// 	if v1 == 0 {
+// 		fmt.Println("Nilai pembagian tidak boleh NOL!")
+// 		return 0
+// 	}
+// 	res := v1 / v2
+// 	return res
+// }
+
+// func sayHello(angkaKe int) {
+// 	fmt.Println("Hallo Wak!", angkaKe)
+// }
+
+func getHello(name string) string {
+	println(name)
+	if name == "" {
+		return "Hallo wak"
+	} else {
+		return "Hallo " + name
 	}
-	res := v1 / v2
-	return res
 }
 
 func main() {
@@ -56,12 +62,20 @@ func main() {
 	// multiple return from function
 	// area, perimeter := rectProps(10.8, 5.6)
 	// fmt.Printf("Area %f Perimeter %f", area, perimeter)
-	bagiDulu := divideValue(7, 2)
-	fmt.Println(bagiDulu)
-	fmt.Println(math.Pi)
+	// bagiDulu := divideValue(7, 2)
+	// fmt.Println(bagiDulu)
+	// fmt.Println(math.Pi)
 	// names := []string{"Andhana", "Rani"}
 	// printMessage("halo", names)
 	//kalo mau kembalikan hanya 1 return value dari function pakai _ (underscore)
 	// area, _ := rectProps(10.8, 5.6) // perimeter variable is discarded
 	// fmt.Printf("Area %f ", area)
+
+	// for i := 1; i <= 10; i++ {
+	// 	sayHello(i)
+	// }
+
+	// result := getHello("")
+	// fmt.Println(result)
+
 }
