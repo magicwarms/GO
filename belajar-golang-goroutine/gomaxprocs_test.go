d31f2c69614bf540aaefeb742bbe68f71b56743b
Andhana Utama <magicwarms@gmail.com>
2022-04-08 11:13:20 +0700

    feat(go routine finished): finished

---
+++ belajar-golang-goroutine/gomaxprocs_test.go
@@ -0,0 +1,21 @@
+package belajar_golang_goroutine
+
+import (
+	"log"
+	"runtime"
+	"testing"
+)
+
+func TestGoMaxProcs(t *testing.T) {
+	totalCPU := runtime.NumCPU()
+	log.Println("Total CPU:", totalCPU)
+	// ini untuk mengubah thread
+	// tak usah diubah gomaxprocs karna default nya sudah optimal
+	runtime.GOMAXPROCS(20) // <-- tak usah diubah-ubah ini
+	totalThread := runtime.GOMAXPROCS(-1)
+	log.Println("Total Thread:", totalThread)
+
+	totalGoroutine := runtime.NumGoroutine()
+	log.Println("total Goroutine:", totalGoroutine)
+
+}
